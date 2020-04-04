5f8f001792a5f082ecdb7d3e09c147d0e7eae7bc
Skayloo <skayloo@yandex.ru>
2020-04-04 22:44:37 +0300

    SecretService

---
+++ SecretService/Hash Windows/Hash.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SGet
+{
+    public partial class Hash : Window
+    {
+        string path = @"C:\SecretService";
+        string name;
+        string status;
+        private MainWindow mainWindow;
+        ObservableCollection<Data> data;
+        public Hash(MainWindow mainWin)
+        {
+            InitializeComponent();
+            mainWindow = mainWin;
+            data = new ObservableCollection<Data>();
+            Checker();
+            ExistedFiles();
+        }
+
+        public class Data
+        {
+            public Data(string name, string time, string status)
+            {
+                this.Name = name;
+                this.Time = time;
+                this.Status = status;
+            }
+            public string Name { get; set; }
+            public string Time { get; set; }
+            public string Status { get; set; }
+        }
+
+        private void Checker()
+        {
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
+        }
+
+        private void btnAddHashFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            DialogResult result = fd.ShowDialog();
+
+            if (result.ToString().Equals("OK"))
+            {
+                name = fd.FileName;
+                AddFiles(name);
+                tbHash.Text = name;
+            }
+            
+        }
+
+        private void ExistedFiles()
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (var item in dir.GetFiles())
+            {
+                string existing = Path.ChangeExtension(item.Name, "exe");
+                status = "Необходима проверка";
+                data.Add(new Data(existing, item.LastWriteTimeUtc.ToString(), status));
+            }
+            hashGrid.ItemsSource = data;
+        }
+
+        private void AddFiles(string name)
+        {
+            string hashed = path + "\\" + Path.GetFileName(name);
+            hashed = Path.ChangeExtension(@hashed, "txt");
+
+            StreamReader st;
+            FileInfo fi = new FileInfo(hashed);
+            if (!File.Exists(hashed))
+            {
+                st = new StreamReader(File.Create(hashed));
+                st.Close();
+                fi.Attributes = FileAttributes.Normal;
+                bool flag_not_to_do_go_to = true;
+                while (flag_not_to_do_go_to)
+                {
+                    try
+                    {
+                        string resolution = ComputeMD5Hash(name);
+ 
+                        using (StreamWriter sw = new StreamWriter(hashed, true, System.Text.Encoding.Default))
+                        {
+                            sw.WriteLine(resolution);
+                            sw.WriteLine(@name);
+                            sw.Close();
+                        }
+                        fi.Attributes = FileAttributes.System | FileAttributes.Hidden | FileAttributes.ReadOnly;
+                        flag_not_to_do_go_to = false;
+                        status = "OK";
+                    }
+                    catch (System.IO.IOException e)
+                    {
+                        flag_not_to_do_go_to = true;
+                    }
+                }
+                data.Add(new Data(Path.GetFileName(name), fi.LastWriteTimeUtc.ToString(), status));
+                hashGrid.ItemsSource = data;
+            }
+        }
+
+
+        private string ComputeMD5Hash(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            using (MD5 md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] checkSum = md5.ComputeHash(fs);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+
+        private void btnCheckSelectedFiles_Click(object sender, RoutedEventArgs e)
+        {
+            if (hashGrid.SelectedItems.Count > 0)
+            {
+                var selectedHash = hashGrid.SelectedItems.Cast<Data>();
+                var hashToClear = new List<Data>();
+                foreach (Data hashing in selectedHash.ToArray())
+                {
+                    DirectoryInfo dir = new DirectoryInfo(path);
+                    foreach (var item in dir.GetFiles())
+                    {
+                        string existing = Path.ChangeExtension(item.Name, "exe");
+                        if (hashing.Name == existing)
+                        {
+                            try
+                            {
+                                string[] read = File.ReadAllLines(item.FullName, System.Text.Encoding.Default);
+                                string n1 = read[0];
+                                string n2 = read[1];
+                                string checking = null;
+                                if (File.Exists(n2))
+                                {
+                                    checking = ComputeMD5Hash(n2);
+                                }
+                                else
+                                {
+                                    MessageBoxResult result = Xceed.Wpf.Toolkit.MessageBox.Show("Невозможно найти файл проверки", "SecretService");
+                                }
+                                if (checking == n1)
+                                {
+                                    hashToClear.Add(hashing);
+                                    status = "OK";
+                                    data.Add(new Data(existing, item.LastWriteTimeUtc.ToString(), status));
+                                }
+                                else
+                                {
+                                    hashToClear.Add(hashing);
+                                    status = "Файл искажен";
+                                    data.Add(new Data(existing, item.LastWriteTimeUtc.ToString(), status));
+                                }
+                            }
+                            catch (Exception ss)
+                            {
+                                string message = ss.ToString();
+                                MessageBoxResult result = Xceed.Wpf.Toolkit.MessageBox.Show(message, "SecretService");
+                            }
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    foreach (var clearhash in hashToClear)
+                    {
+                        data.Remove(clearhash);
+                    }
+                }
+                hashGrid.ItemsSource = data;
+            }
+        }
+    }
+}
---
+++ SecretService/Install/InstallManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.IO;
+
+namespace SGet.Install
+{
+    class InstallManager
+    {
+        public void Installation(string name, string parameters)
+        {
+            try
+            {
+                ProcessStartInfo iInstall = new ProcessStartInfo();
+                iInstall.FileName = @name;
+                iInstall.Arguments = parameters;
+                Process inst = Process.Start(iInstall);
+                inst.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                string message = e.ToString();
+                MessageBoxResult result = Xceed.Wpf.Toolkit.MessageBox.Show(message, "SecretService");
+            }
+        }
+
+        public string Parameters(string name, string path)
+        {
+            string appDataPath = Path.GetDirectoryName(Environment.CurrentDirectory);
+            string folderName = "nbase\\nfdc.exe";
+            string folderPath = System.IO.Path.Combine(appDataPath, folderName);
+            ProcessStartInfo pr = new ProcessStartInfo();
+            pr.FileName = @folderPath;
+            pr.Arguments = @name;
+            pr.UseShellExecute = false;
+            pr.RedirectStandardOutput = true;
+            pr.CreateNoWindow = true;
+
+            Process p = Process.Start(pr);
+            string ss = null;
+            ss = p.StandardOutput.ReadToEnd();
+            string installer = null;
+            installer = getBetween(ss, "Installer:", "(");
+            string linker = null;
+            linker = getBetween(ss, "Linker:", "(");
+            string e = Path.GetExtension(@name);
+            
+            string parameters = null;
+            if (installer == " InstallShield")
+            {
+                parameters = "/s /v\"/qb REBOOT=ReallySuppress";
+                if (path != null)
+                {
+                    parameters += " TARGETDIR=\"" + path + "\"\"";
+                }
+                else
+                {
+                    parameters += "\"";
+                }
+            }
+            else if (installer == " Inno Setup")
+            {
+                parameters = "/SILENT /NORESTART";
+                if (path != null)
+                {
+                    parameters += " /DIR=\"" + path + "\"";
+                }
+            }
+            else if (installer == " Nullsoft Scriptable Install System")
+            {
+                parameters = "/S";
+                if (path != null)
+                {
+                    parameters += " /D=" + path;
+                }
+            }
+            else if (installer == " WISE Installer")
+            {
+                parameters = "/s";
+            }
+            else if (e == ".msi")
+            {
+                parameters = "/qb REBOOT=ReallySuppress";
+                if (path != null)
+                {
+                    parameters += " TARGETDIR=\"" + path + "\"\"";
+                }
+            }
+            else if (e == ".msi" && installer==" InstallShield")
+            {
+                parameters = "/s /v\" /qb REBOOT=ReallySuppress\"";
+            }
+            else if (e == ".msu")
+            {
+                parameters = "/quiet /norestart";
+            }
+            else if (installer == "")
+            {
+                parameters = "";
+            }
+            p.WaitForExit();
+            return parameters;
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
