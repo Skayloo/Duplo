51c216bf76766c1e5c92cd0ccc408214121aa14b
Skayloo <skayloo@yandex.ru>
2020-04-14 23:55:20 +0300

    new file:   SecretService/SecretService.sln

---
+++ SecretService/SecretService/FastClick Windows/PathEdit.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace SGet
+{
+    /// <summary>
+    /// Логика взаимодействия для PathEdit.xaml
+    /// </summary>
+    public partial class PathEdit : Window
+    {
+        private MainWindow mainWindow;
+        public PathEdit(MainWindow mainWin)
+        {
+            InitializeComponent();
+            mainWindow = mainWin;
+        }
+    }
+}
